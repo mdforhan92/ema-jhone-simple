9a5a8a966ab5959b883f97aa1090eeb349b5ce52
mdforhan92 <mdforhan92@gmail.com>
2023-03-27 19:19:16 +0600

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './component/Header/Header'
+import Shop from './component/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className="App">
       
-      <Header />
+      <Header></Header>
+      <Shop></Shop>
     </div>
   )
 }
